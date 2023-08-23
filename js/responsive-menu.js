5a41447860412477ef256f820b52ca72f74abac0
vgabrielk <vfelix1553@gmail.com>
2023-08-23 01:47:26 -0300

    :sparkles: Criando novas secoes e ajuste de erros

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -3,83 +3,62 @@ document.addEventListener('DOMContentLoaded', () => {
   const navList = document.querySelector('.nav-list');
   const mobileMenu = document.querySelector('.mobile-menu');
   const navLinks = document.querySelectorAll('.nav-list li');
+  const header = document.querySelector('.header');
+  const scrollThreshold = 200; // Altura de rolagem em pixels para fixar o cabeçalho
 
-  // Função para alterar a logo para uma versão menor
-  function alterarLogo() {
-    const logoImg = document.querySelector('.logo img');
-    if (window.innerWidth <= 768) {
-      logoImg.src = '../assets/img/logo-responsiva.png'; // Substitua pelo caminho da nova imagem
-      logoImg.height = '34';
-      logoImg.width = '60';
-    } else if (window.innerWidth >= 768) {
-      logoImg.src = '../assets/img/logo-desktop.png'; // Substitua pelo caminho da nova imagem
-      logoImg.height = '80';
-      logoImg.width = '80';
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= scrollThreshold) {
+      header.classList.add('fixed-header', 'fixed-header-animation');
+    } else {
+      header.classList.remove('fixed-header', 'fixed-header-animation');
     }
-  } 
+  });
 
-  // Chama a função para alterar a logo quando a tela é redimensionada
-  window.addEventListener('resize', alterarLogo);
-
-  class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = 'active';
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
-    
-    addClickEvent() {
-      this.mobileMenu.addEventListener('click', this.handleClick);
-    }
-    
-    animateLinks() {
-      this.navLinks.forEach((link) => {
-        link.style.animation
+  function animateLinks() {
+    navLinks.forEach((link) => {
+      link.style.animation
         ? (link.style.animation = '')
         : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
-      });
-    }
-
-    handleClick() {
-      this.animateLinks();
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-
-      if (!this.navList.classList.contains(this.activeClass)) {
-        this.navLinks.forEach(link => {
-          link.style.animation = '';
-        });
-      }
-
-      this.navLinks.forEach(link => {
-        link.classList.toggle(this.activeClass);
-      });
-    }
-    
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
+    });
   }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li"
-  );
-  
-  mobileNavbar.init();
-  
+
+  function handleClick() {
+    animateLinks();
+    navList.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+
+    if (!navList.classList.contains('active')) {
+      navLinks.forEach(link => {
+        link.style.animation = '';
+      });
+    }
+
+    navLinks.forEach(link => {
+      link.classList.toggle('active');
+    });
+  }
+
+  if (mobileMenu) {
+    mobileMenu.addEventListener('click', handleClick);
+  }
+
   closeMenu.addEventListener('click', () => {
+    closeMobileMenu();
+  });
+
+  // Função para fechar o menu móvel
+  function closeMobileMenu() {
     navList.classList.remove('active');
     mobileMenu.classList.remove('active');
     navLinks.forEach(link => {
       link.style.animation = '';
+    });
+  }
+
+  // Adicione um evento de clique para cada link de navegação
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      closeMobileMenu();
     });
   });
 });
