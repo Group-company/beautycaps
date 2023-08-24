3aa8aaf5add14ea3fd176c2689569eac89ba5d66
vgabrielk <vfelix1553@gmail.com>
2023-08-24 04:03:21 -0300

    solucionando problema do menu

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -1,63 +1,67 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const closeMenu = document.querySelector('.close-menu');
-  const navList = document.querySelector('.nav-list');
-  const mobileMenu = document.querySelector('.mobile-menu');
-  const navLinks = document.querySelectorAll('.nav-list li');
-  const header = document.querySelector('.header');
+document.addEventListener("DOMContentLoaded", () => {
+  const closeMenu = document.querySelector(".close-menu");
+  const navList = document.querySelector(".nav-list");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const navLinks = document.querySelectorAll(".nav-list li");
+  const header = document.querySelector(".header");
   const scrollThreshold = 200; // Altura de rolagem em pixels para fixar o cabeçalho
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     if (window.scrollY >= scrollThreshold) {
-      header.classList.add('fixed-header', 'fixed-header-animation');
+      header.classList.add("fixed-header", "fixed-header-animation");
     } else {
-      header.classList.remove('fixed-header', 'fixed-header-animation');
+      header.classList.remove("fixed-header", "fixed-header-animation");
     }
   });
 
   function animateLinks() {
     navLinks.forEach((link) => {
       link.style.animation
-        ? (link.style.animation = '')
+        ? (link.style.animation = "")
         : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`);
     });
   }
 
   function handleClick() {
     animateLinks();
-    navList.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
+    navList.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
 
-    if (!navList.classList.contains('active')) {
-      navLinks.forEach(link => {
-        link.style.animation = '';
+    if (!navList.classList.contains("active")) {
+      navLinks.forEach((link) => {
+        link.style.animation = "";
       });
     }
+    else {
+      mobileMenu.style.display = " none";
+    }
 
-    navLinks.forEach(link => {
-      link.classList.toggle('active');
+    navLinks.forEach((link) => {
+      link.classList.toggle("active");
     });
   }
 
   if (mobileMenu) {
-    mobileMenu.addEventListener('click', handleClick);
+    mobileMenu.addEventListener("click", handleClick);
   }
 
-  closeMenu.addEventListener('click', () => {
+  closeMenu.addEventListener("click", () => {
     closeMobileMenu();
   });
 
   // Função para fechar o menu móvel
   function closeMobileMenu() {
-    navList.classList.remove('active');
-    mobileMenu.classList.remove('active');
-    navLinks.forEach(link => {
-      link.style.animation = '';
+    mobileMenu.style.display = "initial";
+    navList.classList.remove("active");
+    mobileMenu.classList.remove("active");
+    navLinks.forEach((link) => {
+      link.style.animation = "";
     });
   }
 
   // Adicione um evento de clique para cada link de navegação
-  navLinks.forEach(link => {
-    link.addEventListener('click', () => {
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
       closeMobileMenu();
     });
   });
